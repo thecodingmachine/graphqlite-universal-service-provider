325a63015cc079d864075d3d8be20c33a0db927c
David Négrier <d.negrier@thecodingmachine.com>
2019-02-04 18:11:04 +0100

    Adding tests

--- tests/GraphQLiteServiceProviderTest.php
+++ tests/GraphQLiteServiceProviderTest.php
@@ -4,11 +4,20 @@ namespace TheCodingMachine;
 
 use PHPUnit\Framework\TestCase;
 use Simplex\Container;
+use TheCodingMachine\GraphQLite\Schema;
 
 class GraphQLiteServiceProviderTest extends TestCase
 {
     public function testServiceProvider(): void
     {
-        $container = new Container([new GraphQLiteServiceProvider()]);
+        $container = new Container([
+            new SymfonyCacheServiceProvider(),
+            new DoctrineAnnotationsServiceProvider,
+            new GraphQLiteServiceProvider()]);
+        $container->set('graphqlite.namespace.types', ['App\\Types']);
+        $container->set('graphqlite.namespace.controllers', ['App\\Controllers']);
+
+        $schema = $container->get(Schema::class);
+        $this->assertInstanceOf(Schema::class, $schema);
     }
 }
